05715efeb8a2569637668a59ff5f59ec41ea2185
Sergio Fabián <checho.500@hotmail.com>
2023-10-11 00:26:07 -0500

    se agregaron las variables privadas

---
+++ JuegoAhorcado/HangmanGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoAhorcado
+{
+    class HangmanGame
+    {
+        private List<string> words = new List<string> { "tanaka", "rodrigo", "renzo", "andre" };
+        private string currentWord;
+        private List<char> guessedLetters;
+        private int lives;
+    }
