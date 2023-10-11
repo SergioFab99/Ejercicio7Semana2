8f066d8bf94c80869f9aee060e67fadda4bbb328
Sergio Fabián <checho.500@hotmail.com>
2023-10-11 00:29:45 -0500

    programa terminado

--- JuegoAhorcado/HangmanGame.cs
+++ JuegoAhorcado/HangmanGame.cs
@@ -19,5 +19,65 @@ namespace JuegoAhorcado
             guessedLetters = new List<char>();
             lives = 10;
         }
+        public void Play()
+        {
+            Console.Clear();
+            Console.WriteLine("Bienvenido al Juego del Ahorcado");
+            Console.WriteLine("Por favor introduzca una letra");
+            char[] wordToGuess = new char[currentWord.Length];
+
+            while (lives > 0)
+            {
+                Console.WriteLine($"Palabra: {MaskedWord(wordToGuess)}");
+                Console.WriteLine($"Vidas restantes: {lives}");
+                Console.Write("Introduce una letra: ");
+                char guess = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+
+                if (guessedLetters.Contains(guess))
+                {
+                    Console.WriteLine("Ya has adivinado esa letra. Prueba otra.");
+                    continue;
+                }
+
+                guessedLetters.Add(guess);
+
+                if (currentWord.Contains(guess))
+                {
+                    for (int i = 0; i < currentWord.Length; i++)
+                    {
+                        if (currentWord[i] == guess)
+                            wordToGuess[i] = guess;
+                    }
+
+                    if (currentWord == new string(wordToGuess))
+                    {
+                        Console.WriteLine($"¡Has ganado! La palabra era '{currentWord}'.");
+                        return;
+                    }
+                }
+                else
+                {
+                    lives--;
+                    Console.WriteLine("Letra incorrecta. Pierdes una vida.");
+                }
+            }
+
+            Console.WriteLine($"¡Has perdido! La palabra era '{currentWord}'.");
+        }
+
+        private string MaskedWord(char[] word)
+        {
+            string masked = new string(word);
+            for (int i = 0; i < masked.Length; i++)
+            {
+                if (masked[i] == 0)
+                    //Substring es un método que se utiliza para obtener una porción o fragmento
+                    //de una cadena de texto existente,
+                    //especificando la posición de inicio y, opcionalmente, la longitud de la subcadena.
+                    masked = masked.Substring(0, i) + '_' + masked.Substring(i + 1);
+            }
+            return masked;
+        }
     }
 }
